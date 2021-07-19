b5a851622732c86aec6378975e3d752eac0a0d44
fernandagonc <fernandarmg@gmail.com>
2021-07-19 19:39:16 -0300

    add modal

--- my-app/src/AulaCard/AulaCard.js
+++ my-app/src/AulaCard/AulaCard.js
@@ -1,9 +1,20 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import React, { Component } from 'react';
+import ConteudoModal from '../Modal/Modal';
 
 export default class AulaCard extends Component {
-    render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: false,
+    }
+  }  
+
+  handleShow = () => {this.setState(!this.state)};      
+
+   render() {
+
       const style = {
         container:{
           margin: '10px',
@@ -23,9 +34,13 @@ export default class AulaCard extends Component {
           </Card.Text>
           <Button  
             href={this.props.link}
-            variant="primary">
+            variant="primary"
+            onClick={handleModal()}
+            >
               {this.props.button}
           </Button>
+
+          {this.state.showModal && <ConteudoModal/> }
         </Card.Body>
       </Card>
       )
